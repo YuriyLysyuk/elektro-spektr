2d259e9c455ea03f344370943dc5ee5e0ff56e5c
lysyuk <lysyuk.y@gmail.com>
2022-08-16 10:08:43 +0300

    add theme setup

--- functions.php
+++ functions.php
@@ -8,3 +8,21 @@
  * @since        1.0.0
  */
 
+/**
+ * Theme setup.
+ *
+ */
+add_action('genesis_setup', 'ly_theme_setup', 15);
+function ly_theme_setup()
+{
+
+	define(
+		'CHILD_THEME_VERSION',
+		filemtime(get_stylesheet_directory() . '/dist/css/main.min.css')
+	);
+
+	add_theme_support('post-thumbnails');
+
+	// Кастомный размер для изображений категорий в списке категорий
+	add_image_size('category-thumb', 480, 256);
+}
