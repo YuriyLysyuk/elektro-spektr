83c04c1d41820089356eb3f82d77adf1a0d09d0d
lysyuk <lysyuk.y@gmail.com>
2022-08-28 11:20:51 +0300

    add index template

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Index
+ *
+ * @package cabel
+ * @author Yuriy Lysyuk
+ * @since 1.0.0
+ **/
+
+// Добавляем обертку для контента
+add_theme_support('genesis-structural-wraps', ['site-inner']);
+
+
+// Initialize Genesis.
+genesis();
