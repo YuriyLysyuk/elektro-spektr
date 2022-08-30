fc28a0a7bd8e7fa03fb5d4ce3fed553099f581fb
lysyuk <lysyuk.y@gmail.com>
2022-08-30 09:52:26 +0300

    add page template

---
+++ page.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Page
+ *
+ * @package cabel
+ * @author Yuriy Lysyuk
+ * @since 1.0.0
+ **/
+
+// Добавляем обертку для контента
+add_theme_support('genesis-structural-wraps', ['site-inner']);
+
+// Выводим блок с хлебными крошками
+add_action('ly_before_site_inner_wrap', 'ly_breadcrumbs_block');
+
+// Initialize Genesis.
+genesis();
